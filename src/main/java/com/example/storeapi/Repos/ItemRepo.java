fa4554679d3b6ffefbdb313e4aed7b201ca5edc8
ElitaKuld <elitakuld@gmail.com>
2023-04-25 13:18:37 +0200

    Uppdaterat GET- and POST-endpoints for Items, lagt in Items.http

--- src/main/java/com/example/storeapi/Repos/ItemRepo.java
+++ src/main/java/com/example/storeapi/Repos/ItemRepo.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemRepo extends JpaRepository<Item, Long> {
-    List<Item> findByName(String name);
-
-    List<Item> findByPrice(double price);
 }
