66d17bfa42af2e93828d345897c4c5454ab9fe66
Martin Cruces <martin76e@yahoo.com>
2023-04-25 08:57:09 +0200

    added getAllOrders

--- src/main/java/com/example/storeapi/Controllers/OrderController.java
+++ src/main/java/com/example/storeapi/Controllers/OrderController.java
@@ -1,4 +1,35 @@
 package com.example.storeapi.Controllers;
 
+import com.example.storeapi.Models.Order;
+import com.example.storeapi.Repos.CustomerRepo;
+import com.example.storeapi.Repos.ItemRepo;
+import com.example.storeapi.Repos.OrderRepo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class OrderController {
+
+    private final OrderRepo orderRepo;
+
+    private final ItemRepo itemRepo;
+
+    private final CustomerRepo customerRepo;
+
+    OrderController(OrderRepo orderRepo, ItemRepo itemRepo, CustomerRepo customerRepo){
+        this.orderRepo=orderRepo;
+        this.itemRepo=itemRepo;
+        this.customerRepo=customerRepo;
+    }
+
+    @RequestMapping("orders")
+    public List<Order> getAllOrders(){
+
+
+        return orderRepo.findAll() ;
+    }
+
+
 }
