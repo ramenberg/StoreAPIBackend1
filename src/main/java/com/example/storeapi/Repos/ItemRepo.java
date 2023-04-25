8bd675fa3f3e185517a7fa61c915a50c065a6c12
ElitaKuld <elitakuld@gmail.com>
2023-04-25 13:18:57 +0200

    Uppdaterat GET- and POST-endpoints for Items, lagt in Items.http

--- src/main/java/com/example/storeapi/Repos/ItemRepo.java
+++ src/main/java/com/example/storeapi/Repos/ItemRepo.java
@@ -3,7 +3,5 @@ package com.example.storeapi.Repos;
 import com.example.storeapi.Models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ItemRepo extends JpaRepository<Item, Long> {
 }
