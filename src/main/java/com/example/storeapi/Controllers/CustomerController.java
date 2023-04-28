1d40d2d585ada2428e8482bd0a170f34ffffa0dc
Emelie Ramenberg <ramenberg@gmail.com>
2023-04-28 08:58:09 +0200

    Skapat ny controller för Thymeleaf.

--- src/main/java/com/example/storeapi/Controllers/CustomerController.java
+++ src/main/java/com/example/storeapi/Controllers/CustomerController.java
@@ -3,14 +3,12 @@ package com.example.storeapi.Controllers;
 import com.example.storeapi.Models.Customer;
 import com.example.storeapi.Repos.CustomerRepo;
 import com.example.storeapi.exceptions.CustomerNotFoundException;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.logging.Logger;
 
-@Controller
+@RestController
 public class CustomerController {
 
     private final Logger log = Logger.getLogger(CustomerController.class.getName());
@@ -20,34 +18,17 @@ public class CustomerController {
         this.customerRepo = customerRepo;
     }
 
-    @GetMapping("/customers")
-    public String customers(Model model) {
-        log.info("GET all /customers");
-        model.addAttribute("customers", this.all());
-        return "customers.html";
+    @GetMapping("/customers/{id}")
+    public Customer one(@PathVariable Long id) {
+        log.info("GET /customers/" + id);
+        return customerRepo.findById(id).orElseThrow(() -> new CustomerNotFoundException(id));
     }
 
     public List<Customer> all() {
         return customerRepo.findAll();
     }
 
-//    // Första versionen
-//    @GetMapping("/customers")
-//    @ResponseBody
-//    public List<Customer> all() {
-//        log.info("GET all /customers");
-//        return customerRepo.findAll();
-//    }
-
-    @GetMapping("/customers/{id}")
-    @ResponseBody
-    public Customer one(@PathVariable Long id) {
-        log.info("GET /customers/" + id);
-        return customerRepo.findById(id).orElseThrow(() -> new CustomerNotFoundException(id));
-    }
-
     @PostMapping("/customers")
-    @ResponseBody
     public Customer newCustomer(@RequestBody Customer newCustomer) {
         // Kontrollera att alla fält är ifyllda
         if (customerDoesNotHaveAllRequiredFields(newCustomer)) {
